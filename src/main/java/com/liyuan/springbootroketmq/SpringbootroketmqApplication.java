beb8e82dd1bbc1e5d504b6183ce113e405f3ae90
weiyuan0727 <weiyuan0727@163.com>
2019-12-02 22:27:13 +0800

    Initial commit

---
+++ src/main/java/com/liyuan/springbootroketmq/SpringbootroketmqApplication.java
@@ -0,0 +1,13 @@
+package com.liyuan.springbootroketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootroketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootroketmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liyuan/springbootroketmq/SpringbootroketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyuan.springbootroketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootroketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
